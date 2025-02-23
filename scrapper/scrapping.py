fa53b8e16f6e4b80152f83e00eb0a258e3d6431d
arsh0429 <158746173+arsh0429@users.noreply.github.com>
2025-02-24 02:21:53 +0530

    updated scraped data

--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -1,28 +1,18 @@
 import os
 import pandas as pd
 from pymongo import MongoClient
-from utils import scrape_yahoo_news
+from utils import scrape_bbc_news,scrape_yahoo_news, scrape_indian_express
 
 yahoo_df = scrape_yahoo_news()
-yahoo_df.to_csv("data/yahoo_news_dataset.csv", index=False)
+yahoo_df.to_csv("data/global/yahoo_news_dataset.csv", index=False)
 print("Yahoo News dataset saved as yahoo_news_dataset.csv")
 
-# data_dir = "data"
-# all_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith('.csv')]
+bbc_df=scrape_bbc_news()
+bbc_df=bbc_df.drop(index=1)
+bbc_df.to_csv("data/global/bbc_news_dataset.csv", index=False)
+print("bbc News dataset saved as bbc_news_dataset.csv")
 
-# df_list = [pd.read_csv(file) for file in all_files]
-# merged_df = pd.concat(df_list, ignore_index=True)
-
-# categories = merged_df['Category'].unique()
-# category_dfs = {category: merged_df[merged_df['Category'] == category] for category in categories}
-
-# client = MongoClient('mongodb://localhost:27017/')
-# db = client['news_database']
-
-# for category, df in category_dfs.items():
-#     collection = db[category] 
-#     records = df.to_dict('records')
-#     collection.insert_many(records)
-#     print(f"Inserted {len(records)} records into {category} collection")
-
-# print("Data has been successfully pushed to MongoDB.")
+indian_express_df=scrape_indian_express()
+indian_express_df=indian_express_df.drop(index=1)
+indian_express_df.to_csv("data/indian/Indian_express_news_dataset.csv", index=False)
+print("bbc News dataset saved as indian_express_news_dataset.csv")
