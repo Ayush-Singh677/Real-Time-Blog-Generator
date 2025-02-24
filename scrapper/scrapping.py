306bd8a76bb1e0e9fa1c5ef0697e24e175e35579
arsh0429 <158746173+arsh0429@users.noreply.github.com>
2025-02-24 13:39:31 +0530

    Updated scrapping.py

--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from pymongo import MongoClient
-from utils import scrape_bbc_news,scrape_yahoo_news, scrape_indian_express
+from utils import scrape_bbc_news,scrape_yahoo_news, scrape_indian_express, scrape_nytimes_world_news
 
 yahoo_df = scrape_yahoo_news()
 yahoo_df.to_csv("data/global/yahoo_news_dataset.csv", index=False)
@@ -15,4 +15,9 @@ print("bbc News dataset saved as bbc_news_dataset.csv")
 indian_express_df=scrape_indian_express()
 indian_express_df=indian_express_df.drop(index=1)
 indian_express_df.to_csv("data/indian/Indian_express_news_dataset.csv", index=False)
-print("bbc News dataset saved as indian_express_news_dataset.csv")
+print("Indian express News dataset saved as indian_express_news_dataset.csv")
+
+nytimes_df=scrape_nytimes_world_news()
+nytimes_df=nytimes_df.drop(index=1)
+nytimes_df.to_csv("data/global/nytimes_news.csv", index=False)
+print("NY Times News dataset saved as nytimes_news.csv")
